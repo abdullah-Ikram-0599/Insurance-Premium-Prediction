e5e8f9a3e3dabfb61e8b8dd7c7d0149d545aecea
Abdullah Ikram <abdullah.ikram.0599@gmail.com>
2024-07-11 14:06:54 +0530

    Hyperparameter tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -5,9 +5,8 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 from src.exception import CustomException
 from src.logger import logging
